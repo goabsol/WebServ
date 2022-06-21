36f4a92932d0fabffe35fcbe5ea6acb4488f1d1e
ael-bagh <ael-bagh@student.1337.ma>
2022-06-21 05:58:36 +0100

    we did a server thing

--- webserv.cpp
+++ webserv.cpp
@@ -7,66 +7,66 @@
 #include <errno.h>
 #include <iostream>
 #include <time.h>
+#include <string>
 
 #define SOCKET int
+void *memeset(void *s, int c, size_t n)
+{
+    char *p = (char *)s;
+    while (n--)
+        *p++ = c;
+    return s;
+}
+
 int main()
 {
-    std::cout << "Configuring local address..." << std::endl;
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-    struct addrinfo *bind_address;
-    getaddrinfo(0, "8080", &hints, &bind_address);
-    std::cout << "Creating socket..." << std::endl;
-    SOCKET socket_listen;
-    socket_listen = socket(bind_address->ai_family,
-    bind_address->ai_socktype, bind_address->ai_protocol);
-    if (socket_listen < 0)
+    struct sockaddr_in johnny; 
+    SOCKET server_fd;
+    int server_port = 8080;
+    ///add error managemement
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0)
     {
-        std::cerr << "Error creating socket: " << strerror(errno) << std::endl;
+        std::cout << "Error opening socket" << std::endl;
         return 1;
     }
-    if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen))
+    memeset(&johnny, 0, sizeof(johnny));
+    johnny.sin_family = AF_INET;
+    johnny.sin_port = htons(server_port);
+    johnny.sin_addr.s_addr = INADDR_ANY;
+    //add error managemement
+    if (bind(server_fd, (struct sockaddr *)&johnny, sizeof(johnny)) < 0)
     {
-        std::cerr << "Error binding socket: " << strerror(errno) << std::endl;
+        std::cout << "Error binding socket" << std::endl;
         return 1;
     }
-    freeaddrinfo(bind_address);
-    std::cout << "Listening..." << std::endl;
-    if (listen(socket_listen, 10) < 0)
+    if (listen(server_fd, 5) < 0)
     {
-        std::cerr << "Error listening: " << strerror(errno) << std::endl;
+        std::cout << "Error listening on socket" << std::endl;
         return 1;
     }
-    std::cout << "Accepting connection..." << std::endl;
-    struct sockaddr_in client_address;
-    socklen_t client_address_length = sizeof(client_address);
-    SOCKET socket_client = accept(socket_listen, (struct sockaddr *)&client_address, &client_address_length);
-    if (socket_client < 0)
+    while (1)
     {
-        std::cerr << "Error accepting connection: " << strerror(errno) << std::endl;
-        return 1;
+        int client_fd;
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_fd < 0)
+        {
+            std::cout << "Error accepting connection" << std::endl;
+            return 1;
+        }
+        char buffer[1024]={0};
+        int valrecv = recv(client_fd,buffer,1024,0);
+        if (valrecv < 0)
+        {
+            std::cout << "Error receiving data" << std::endl;
+            return 1;
+        }
+        std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+        send(client_fd,hello.c_str(),hello.length(),0);
+        close(client_fd);
     }
-    std::cout << "Client is connected" << std::endl;
-    char buffer[100];
-    getnameinfo((struct sockaddr *)&client_address, client_address_length, buffer, sizeof(buffer), 0, 0, NI_NUMERICHOST);
-    std::cout << "Client is connected from " << buffer << std::endl;
-    std::cout << "Reading request..." << std::endl;
-    char request[1024];
-    int bytes_read = recv(socket_client, request, sizeof(request), 0);
-    printf("%.*s", bytes_read, request);
-    std::cout << "sending response..." << std::endl;
-    const char* response = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/plain\r\n\r\nLocal time is: ";
-    int bytes_sent = send(socket_client, response, strlen(response), 0);
-    //printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
-    time_t timer;
-    time(&timer);
-    char *time_msg = ctime(&timer);
-    bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
-    //printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
-    close(socket_client);
-    close(socket_listen);
-    std::cout << "Done." << std::endl;
+    close(server_fd);
+    return 0;
 }
