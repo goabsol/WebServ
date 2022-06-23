676020b6e21cee75b0df1e4b23f3e13c12c3bbdc
Aymane RHALLAB <aymanegriffin@gmail.com>
2022-06-23 10:15:31 +0100

    multiplexing without timeout done

--- webserv.cpp
+++ webserv.cpp
@@ -32,19 +32,20 @@ int main()
 	s.port.push_back(std::make_pair(0, 8080));
 	s.port.push_back(std::make_pair(0, 8081));
 	s.port.push_back(std::make_pair(0, 8082));
-	s.name = "server1";
+	s.name = "server 1";
 	servers.push_back(s);
 	s.port.clear();
 	s.port.push_back(std::make_pair(0, 8083));
 	s.port.push_back(std::make_pair(0, 8084));
 	s.port.push_back(std::make_pair(0, 8085));
-	s.name = "server2";
+	s.name = "server 2";
 	servers.push_back(s);
     struct sockaddr_in johnny; 
     SOCKET server_fd;
-	fd_set our_sockets;
-	FD_ZERO(&our_sockets);
+	fd_set read_fd;
+	FD_ZERO(&read_fd);
 	SOCKET max_fd = 0;
+	const int enable = 1;
 	for (int i = 0; i < servers.size(); i++)
 	{
 		for (int j = 0; j < servers[i].port.size(); j++)
@@ -55,6 +56,8 @@ int main()
 				std::cout << "Error opening socket" << std::endl;
 				return 1;
 			}
+			if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+				std::cout << "setsockopt(SO_REUSEADDR) failed" << std::endl;
 			memeset(&johnny, 0, sizeof(johnny));
 			johnny.sin_family = AF_INET;
 			johnny.sin_port = htons(servers[i].port[j].second);
@@ -69,27 +72,30 @@ int main()
 				std::cout << "Error listening on socket" << std::endl;
 				return 1;
 			}
-			FD_SET(server_fd, &our_sockets);
+			FD_SET(server_fd, &read_fd);
 			if (server_fd > max_fd)
 				max_fd = server_fd;
-			std::cout << server_fd << std::endl;
 			m[server_fd] = servers[i];
 		}
 	}
-	fd_set master = our_sockets;
+	fd_set master = read_fd;
+	fd_set write_fd;
+	FD_ZERO(&write_fd);
 	while (1)
 	{
-		fd_set copy = our_sockets;
-		if (select(max_fd + 1, &copy, NULL, NULL, NULL) < 0)
+		//add fd_write;
+		fd_set rcopy = read_fd;
+		fd_set wcopy = write_fd;
+		if (select(max_fd + 1, &rcopy, &wcopy, NULL, NULL) < 0)
 		{
 			std::cout << "Error in select" << std::endl;
 			return 1;
 		}
 		for (int i = 1; i <= max_fd; i++)
 		{
-			if (FD_ISSET(i, &copy))
+			if (FD_ISSET(i, &rcopy))
 			{
-				std::cout << "Socket " << i << " of server " << m[i].name << " is ready" << std::endl;
+				std::cout << "Socket " << i << " of " << m[i].name << " is ready for reading" << std::endl;
 				if (FD_ISSET(i, &master))
 				{
 					std::cout << "New connection" << std::endl;
@@ -101,10 +107,11 @@ int main()
 						std::cout << "Error accepting connection" << std::endl;
 						return 1;
 					}
-					FD_SET(client_fd, &our_sockets);
+					if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+						std::cout << "setsockopt(SO_REUSEADDR) failed" << std::endl;
+					FD_SET(client_fd, &read_fd);
 					if (client_fd > max_fd)
 						max_fd = client_fd;
-					std::cout << client_fd << std::endl;
 					m[client_fd] = m[i];
 					i = client_fd;
 				}
@@ -112,51 +119,29 @@ int main()
 				char buffer[1024];
 				memset(buffer, 0, 1024);
 				int bytes_read = recv(i, buffer, 1024, 0);
-				if (bytes_read < 0)
+				if (bytes_read <= 0)
 				{
-					std::cout << "Error reading data" << std::endl;
-					return 1;
-				}
-				else if (bytes_read == 0)
-				{
-					std::cout << "Connection closed" << std::endl;
-					FD_CLR(i, &our_sockets);
+					std::cout << (bytes_read < 0?"Error reading data ":"") << "Connection closed" << std::endl;
+					FD_CLR(i, &read_fd);
 					close(i);
 				}
 				else
 				{
 					std::cout << "Data: " << buffer << std::endl;
-					std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-        			send(i,hello.c_str(),hello.length(),0);
-					std::cout << "Data sent" << std::endl;
+					FD_CLR(i, &read_fd);
+					FD_SET(i, &write_fd);
 				}
+			}
+			else if (FD_ISSET(i, &write_fd))
+			{
+				std::cout << "Socket " << i << " of " << m[i].name << " is ready for writing" << std::endl;
+				std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+				send(i,hello.c_str(),hello.length(),0);
+				std::cout << "Data sent" << std::endl;
+				FD_CLR(i, &write_fd);
+				FD_SET(i, &read_fd);
 			}
 		}
 	}
 	return 0;
 }
-    // while (1)
-    // {
-    //     int client_fd;
-    //     struct sockaddr_in client_addr;
-    //     socklen_t client_addr_len = sizeof(client_addr);
-    //     client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-    //     if (client_fd < 0)
-    //     {
-    //         std::cout << "Error accepting connection" << std::endl;
-    //         return 1;
-    //     }
-    //     char buffer[1024]={0};
-    //     int valrecv = recv(client_fd,buffer,1024,0);
-    //     if (valrecv < 0)
-    //     {
-    //         std::cout << "Error receiving data" << std::endl;
-    //         return 1;
-    //     }
-    //     std::string hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
-    //     send(client_fd,hello.c_str(),hello.length(),0);
-    //     close(client_fd);
-    // }
-    // close(server_fd);
-//     return 0;
-// }
