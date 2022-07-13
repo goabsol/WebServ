6f2c6eb4eb691bfed4d4ffd40695832d2e8ec4f0
Aymane RHALLAB <aymanegriffin@gmail.com>
2022-07-13 14:48:21 +0100

    bit of cleanup

--- request/ClientRequest.hpp
+++ request/ClientRequest.hpp
@@ -55,7 +55,7 @@ class ClientRequest
 		Server_T server;
 		Location_T current_location;
 		std::string current_location_path;
-		timeval start_time
+		timeval start_time;
 		size_t size;
 		bool size_set;
 		bool expect_newline;
